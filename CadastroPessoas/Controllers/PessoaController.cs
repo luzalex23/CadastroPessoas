d5c30dae800945ee75c6cb22cdbb7343bd5dff0e
luzalex23 <alexluzendreyravi@gmail.com>
2023-10-26 15:13:43 -0300

    Add class controller com os metodos get e post e Model View

---
+++ CadastroPessoas/Controllers/PessoaController.cs
@@ -0,0 +1,32 @@
+﻿using CadastroPessoas.Model;
+using CadastroPessoas.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroPessoas.Controllers
+{
+    [ApiController]
+    [Route("api/vc/pessoa")]
+    public class PessoaController : ControllerBase
+    {
+        private readonly IPessoasRespository _pessoasRespository;
+
+        public PessoaController(IPessoasRespository pessoasRespository)
+        {
+            _pessoasRespository = pessoasRespository ?? throw new ArgumentNullException(nameof(pessoasRespository)); 
+        }
+
+        [HttpPost]
+        public IActionResult Add(PessoaViewModel pessoaView)
+        {
+            var p = new Pessoas(pessoaView.Name, pessoaView.Age, null);
+            _pessoasRespository.Add(p);
+            return Ok();
+        }
+        [HttpGet]
+         public IActionResult FindAll() 
+        {
+            var p = _pessoasRespository.Get();
+            return Ok(p);
+        }
+    }
+}
---
+++ CadastroPessoas/ViewModel/PessoaViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CadastroPessoas.ViewModel
+{
+    public class PessoaViewModel
+    {
+        public string Name {  get; set; }
+        public int Age {  get; set; }
+
+       // public string? Photo {  get; set; }
+    }
+}
